831fd541d95115e499da05726972f140ed6a7b1e
Suvash Thapaliya <suvash@gmail.com>
2015-01-31 22:38:32 +0100

    Packet Struct gets valid? checking

--- lib/hulaaki/control/packet.ex
+++ lib/hulaaki/control/packet.ex
@@ -1,3 +1,23 @@
 defmodule Hulaaki.Control.Packet do
   defstruct [:type]
+
+  @valid_types Enum.into([:CONNECT,
+                          :CONNACK,
+                          :PUBLISH,
+                          :PUBACK,
+                          :PUBREC,
+                          :PUBREL,
+                          :PUBCOMP,
+                          :SUBSCRIBE,
+                          :SUBACK,
+                          :UNSUBSCRIBE,
+                          :UNSUBACK,
+                          :PINGREC,
+                          :PINGRESP,
+                          :DISCONNECT
+                         ], HashSet.new)
+
+  def valid?(packet) do
+    Set.member?(@valid_types, packet.type)
+  end
 end
--- test/hulaaki/control/packet_test.exs
+++ test/hulaaki/control/packet_test.exs
@@ -1,7 +1,65 @@
 defmodule Hulaaki.Control.PacketTest do
   use ExUnit.Case
+  alias Hulaaki.Control.Packet, as: Packet
 
-  test "additions" do
-    assert 1+1 = 2
+  test "invalid packet type" do
+    expected = Packet.valid? %Packet{type: :NONSENSE}
+    refute expected
+  end
+
+  test "CONNECT packet type is valid" do
+    assert Packet.valid?(%Packet{type: :CONNECT})
+  end
+
+  test "CONNACK packet type is valid" do
+    assert Packet.valid?(%Packet{type: :CONNACK})
+  end
+
+  test "PUBLISH packet type is valid" do
+    assert Packet.valid?(%Packet{type: :PUBLISH})
+  end
+
+  test "PUBACK packet type is valid" do
+    assert Packet.valid?(%Packet{type: :PUBACK})
+  end
+
+  test "PUBREC packet type is valid" do
+    assert Packet.valid?(%Packet{type: :PUBREC})
+  end
+
+  test "PUBREL packet type is valid" do
+    assert Packet.valid?(%Packet{type: :PUBREL})
+  end
+
+  test "PUBCOMP packet type is valid" do
+    assert Packet.valid?(%Packet{type: :PUBCOMP})
+  end
+
+  test "SUBSCRIBE packet type is valid" do
+    assert Packet.valid?(%Packet{type: :SUBSCRIBE})
+  end
+
+  test "SUBACK packet type is valid" do
+    assert Packet.valid?(%Packet{type: :SUBACK})
+  end
+
+  test "UNSUBSCRIBE packet type is valid" do
+    assert Packet.valid?(%Packet{type: :UNSUBSCRIBE})
+  end
+
+  test "UNSUBACK packet type is valid" do
+    assert Packet.valid?(%Packet{type: :UNSUBACK})
+  end
+
+  test "PINGREC packet type is valid" do
+    assert Packet.valid?(%Packet{type: :PINGREC})
+  end
+
+  test "PINGRESP packet type is valid" do
+    assert Packet.valid?(%Packet{type: :PINGRESP})
+  end
+
+  test "DISCONNECT packet type is valid" do
+    assert Packet.valid?(%Packet{type: :DISCONNECT})
   end
 end
