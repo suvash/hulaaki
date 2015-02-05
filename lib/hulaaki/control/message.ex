74b9e39fbf323853b3aa54f8df3211c18b4150e0
Suvash Thapaliya <suvash@gmail.com>
2015-02-06 00:09:23 +0100

    Adds Message structs for all sorts of control packets

---
+++ lib/hulaaki/control/message.ex
@@ -0,0 +1,227 @@
+defmodule Hulaaki.Control.Message do
+
+  # TODO: VERIFY and DROP type in structs after functions to encode structs
+  # TODO: ADD specs to functions as well
+  # TODO: ADD documentation ExDoc style
+
+  defmodule Connect do
+    @type t :: %__MODULE__{
+              client_id: non_neg_integer,
+              username: String.t,
+              password: String.t,
+              will_topic: String.t,
+              will_message: String.t,
+              will_qos: 0|1|2,
+              will_retain: 0|1,
+              clean_session: 0|1,
+              keep_alive: integer,
+              type: atom}
+    defstruct [:client_id,
+               :username,
+               :password,
+               :will_topic,
+               :will_message,
+               :will_qos,
+               :will_retain,
+               :clean_session,
+               :keep_alive,
+               type: :CONNECT]
+  end
+
+  def connect(client_id, username, password,
+              will_topic, will_message, will_qos,
+              will_retain, clean_session, keep_alive)
+    when is_integer(client_id)
+    and client_id > 0
+    and is_binary(username)
+    and is_binary(password)
+    and is_binary(will_topic)
+    and is_binary(will_message)
+    and (will_qos == 0 or will_qos == 1 or will_qos == 2)
+    and (will_retain == 0 or will_retain == 1)
+    and (clean_session == 0 or clean_session == 1)
+    and is_integer(keep_alive) do
+
+      %Connect{client_id: client_id, username: username, password: password,
+               will_topic: will_topic, will_message: will_message,
+               will_qos: will_qos, will_retain: will_retain,
+               clean_session: clean_session, keep_alive: keep_alive}
+  end
+
+  defmodule ConnAck do
+    @type t :: %__MODULE__{session_present: 0|1, return_code: 1|2|3|4|5, type: atom}
+    defstruct [:session_present, :return_code, type: :CONNACK]
+  end
+
+  def connect_ack(session_present, return_code)
+    when (session_present == 0 or session_present == 1)
+    and (return_code == 1 or return_code == 2 or
+         return_code == 3 or return_code == 4 or
+         return_code == 5) do
+
+      %ConnAck{session_present: session_present, return_code: return_code}
+  end
+
+  defmodule Publish do
+    @type t :: %__MODULE__{
+               id: non_neg_integer,
+               topic: String.t,
+               message: String.t,
+               dup: 0|1,
+               qos: 0|1|2,
+               retain: 0|1,
+               type: atom}
+    defstruct [:id, :topic, :message, :dup, :qos, :retain, type: :PUBLISH]
+  end
+
+  def publish(packet_id, topic, message, dup, qos, retain)
+    when is_integer(packet_id)
+    and packet_id > 0
+    and is_binary(topic)
+    and is_binary(message)
+    and (dup == 0 or dup == 1)
+    and (qos == 0 or qos == 1 or qos == 2)
+    and (retain == 0 or retain == 1) do
+
+      %Publish{id: packet_id, topic: topic, message: message,
+               dup: dup, qos: qos, retain: retain}
+  end
+
+  defmodule PubAck do
+    @type t :: %__MODULE__{id: non_neg_integer, type: atom}
+    defstruct [:id, type: :PUBACK]
+  end
+
+  def publish_ack(packet_id)
+    when is_integer(packet_id)
+    and packet_id > 0 do
+
+      %PubAck{id: packet_id}
+  end
+
+  defmodule PubRec do
+    @type t :: %__MODULE__{id: non_neg_integer, type: atom}
+    defstruct [:id, type: :PUBREC]
+  end
+
+  def publish_receive(packet_id)
+    when is_integer(packet_id)
+    and packet_id > 0 do
+
+      %PubRec{id: packet_id}
+  end
+
+  defmodule PubRel do
+    @type t :: %__MODULE__{id: non_neg_integer, type: atom}
+    defstruct [:id, type: :PUBREL]
+  end
+
+  def publish_release(packet_id)
+    when is_integer(packet_id)
+    and packet_id > 0 do
+
+      %PubRel{id: packet_id}
+  end
+
+  defmodule PubComp do
+    @type t :: %__MODULE__{id: non_neg_integer, type: atom}
+    defstruct [:id, type: :PUBCOMP]
+  end
+
+  def publish_complete(packet_id)
+    when is_integer(packet_id)
+    and packet_id > 0 do
+
+      %PubComp{id: packet_id}
+  end
+  defmodule Subscribe do
+    @type t :: %__MODULE__{
+               id: non_neg_integer,
+               topics: list(String.t),
+               requested_qoses: list(0|1|2),
+               type: atom}
+    defstruct [:id, :topics, :requested_qoses, type: :SUBSCRIBE]
+  end
+
+  def subscribe(packet_id, topics, requested_qoses)
+    when is_integer(packet_id)
+    and packet_id > 0
+    and is_list(topics)
+    and is_list(requested_qoses) do
+      clean_topics = Enum.filter(topics, fn(x) -> is_binary(x) end)
+      valid_qos? = fn(x) -> (x == 0 or x == 1 or x == 2) end
+      clean_qoses = Enum.filter(requested_qoses, valid_qos?)
+
+      %Subscribe{id: packet_id, topics: clean_topics, requested_qoses: clean_qoses}
+  end
+  defmodule SubAck do
+    @type t :: %__MODULE__{
+               id: non_neg_integer,
+               granted_qoses: list(0|1|2|128),
+               type: atom}
+    defstruct [:id, :granted_qoses, type: :SUBACK]
+  end
+
+  def subscribe_ack(packet_id, granted_qoses)
+    when is_integer(packet_id)
+    and packet_id > 0
+    and is_list(granted_qoses) do
+      valid_qos? = fn(x) -> (x == 0 or x == 1 or x == 2 or x == 128) end
+      clean_qoses = Enum.filter(granted_qoses, valid_qos?)
+
+      %SubAck{id: packet_id, granted_qoses: clean_qoses}
+  end
+
+  defmodule Unsubscribe do
+    @type t :: %__MODULE__{id: non_neg_integer, topics: list(String.t), type: atom}
+    defstruct [:id, :topics, type: :UNSUBSCRIBE]
+  end
+
+  def unsubscribe(packet_id, topics)
+    when is_integer(packet_id)
+    and packet_id > 0
+    and is_list(topics) do
+      clean_topics = Enum.filter(topics, fn(x) -> is_binary(x) end)
+
+      %Unsubscribe{id: packet_id, topics: clean_topics}
+  end
+
+  defmodule UnsubAck do
+    @type t :: %__MODULE__{id: non_neg_integer, type: atom}
+    defstruct [:id, type: :UNSUBACK]
+  end
+
+  def unsubscribe_ack(packet_id)
+    when is_integer(packet_id)
+    and packet_id > 0 do
+
+      %UnsubAck{id: packet_id}
+  end
+
+  defmodule PingReq do
+    @type t :: %__MODULE__{type: atom}
+    defstruct type: :PINGREQ
+  end
+
+  def ping_request do
+    %PingReq{}
+  end
+
+  defmodule PingResp do
+    @type t :: %__MODULE__{type: atom}
+    defstruct type: :PINGRESP
+  end
+
+  def ping_response do
+    %PingResp{}
+  end
+
+  defmodule Disconnect do
+    @type t :: %__MODULE__{type: atom}
+    defstruct type: :DISCONNECT
+  end
+
+  def disconnect do
+    %Disconnect{}
+  end
+end
---
+++ test/hulaaki/control/message_test.exs
@@ -0,0 +1,147 @@
+defmodule Hulaaki.Control.MessageTest do
+  use ExUnit.Case
+  alias Hulaaki.Control.Message, as: Message
+
+  test "connect build a Connect message struct" do
+    id = :random.uniform(999999)
+    username = "test-user"
+    password = "test-password"
+    will_topic = "will-topic"
+    will_message = "will-message"
+    will_qos = 0
+    will_retain = 1
+    clean_session = 0
+    keep_alive = 10
+    expected = %Message.Connect{client_id: id,
+                                username: username,
+                                password: password,
+                                will_topic: will_topic,
+                                will_message: will_message,
+                                will_qos: will_qos,
+                                will_retain: will_retain,
+                                clean_session: clean_session,
+                                keep_alive: keep_alive}
+    received = Message.connect(id, username, password,
+                               will_topic, will_message, will_qos,
+                               will_retain, clean_session, keep_alive)
+
+    assert expected == received
+  end
+
+  test "connect_ack builds a ConnAck message struct" do
+    session_present = 0
+    return_code = 3
+    expected = %Message.ConnAck{session_present: session_present,
+                                return_code: return_code}
+    received = Message.connect_ack(session_present, return_code)
+
+    assert expected == received
+  end
+
+  test "publish builds a Publish message struct" do
+    id = :random.uniform(999999)
+    topic = "nice_topic"
+    message = " a short message"
+    dup = 0
+    qos = 2
+    retain = 1
+    expected = %Message.Publish{id: id, topic: topic,
+                                message: message, dup: dup,
+                                qos: qos, retain: retain}
+    received = Message.publish(id, topic, message, dup, qos, retain)
+
+    assert expected == received
+  end
+
+  test "publish_ack builds a PubAck message struct" do
+    id = :random.uniform(999999)
+    expected = %Message.PubAck{id: id}
+    received = Message.publish_ack(id)
+
+    assert expected == received
+  end
+
+  test "publish_received builds a PubRec message struct" do
+    id = :random.uniform(999999)
+    expected = %Message.PubRec{id: id}
+    received = Message.publish_receive(id)
+
+    assert expected == received
+  end
+
+  test "publish_release builds a PubRel message struct" do
+    id = :random.uniform(999999)
+    expected = %Message.PubRel{id: id}
+    received = Message.publish_release(id)
+
+    assert expected == received
+  end
+
+  test "publish_complete builds a PubComp message struct" do
+    id = :random.uniform(999999)
+    expected = %Message.PubComp{id: id}
+    received = Message.publish_complete(id)
+
+    assert expected == received
+  end
+
+  test "subscribe build a Subscribe message struct" do
+    id = :random.uniform(999999)
+    topics = [1,2,"hello","cool"]
+    qoses = ["hello", -1, 0, 1, 2, 3, 12.34, 5, 123, 128]
+    expected = %Message.Subscribe{id: id,
+                                  topics: ["hello", "cool"],
+                                  requested_qoses: [0, 1, 2]}
+    received = Message.subscribe(id, topics, qoses)
+
+    assert expected == received
+
+  end
+
+  test "subscribe_ack builds a SubAck message struct" do
+    id = :random.uniform(999999)
+    qoses = ["hello", -1, 0, 1, 2, 3, 12.34, 5, 123, 128]
+    expected = %Message.SubAck{id: id, granted_qoses: [0, 1, 2, 128]}
+    received = Message.subscribe_ack(id, qoses)
+
+    assert expected == received
+  end
+
+  test "unsubscribe builds a Unsubscribe message struct" do
+    id = :random.uniform(999999)
+    topics = [1,2,"hello","cool"]
+    expected = %Message.Unsubscribe{id: id, topics: ["hello", "cool"]}
+    received = Message.unsubscribe(id, topics)
+
+    assert expected == received
+  end
+
+  test "unsubscribe_ack builds a UnsubAck message struct" do
+    id = :random.uniform(999999)
+    expected = %Message.UnsubAck{id: id}
+    received = Message.unsubscribe_ack(id)
+
+    assert expected == received
+  end
+
+  test "ping_request builds a PingReq message struct" do
+    expected = %Message.PingReq{}
+    received = Message.ping_request
+
+    assert expected == received
+  end
+
+  test "ping_response builds a PingResponse message struct" do
+    expected = %Message.PingResp{}
+    received = Message.ping_response
+
+    assert expected == received
+  end
+
+  test "disconnect builds a Disconnect message struct" do
+    expected = %Message.Disconnect{}
+    received = Message.disconnect
+
+    assert expected == received
+  end
+end
