d0f046e776aae1c3ff5201b4b0d6059050880047
Suvash Thapaliya <suvash@gmail.com>
2015-02-12 23:07:43 +0100

    Decode remaining length from encoded value

--- lib/hulaaki/decoder.ex
+++ lib/hulaaki/decoder.ex
@@ -1,5 +1,6 @@
 defmodule Hulaaki.Decoder do
   alias Hulaaki.Message, as: Message
+  use Bitwise
 
   def decode(<<packet_type_value::bits-4, _rest::bits>>) do
     case packet_type_value do
@@ -10,6 +11,25 @@ defmodule Hulaaki.Decoder do
   end
 
   def decode_ping_request do
+  def decode_remaining_length(<<0>>), do: 0
+
+  def decode_remaining_length(bytes) do
+    decode_remaining_length(bytes, 0, 1)
+  end
+
+  defp decode_remaining_length(<<encodedValue, rest::binary>>,
+                               accumulator, multiplier)
+  when (multiplier <= 2_097_152) do
+    remaining_bytes? = fn(x) -> (Bitwise.band(x, 128) != 0) end
+
+    decodedValue = Bitwise.band(encodedValue, 127) * multiplier
+
+    if remaining_bytes?.(encodedValue) do
+      decode_remaining_length(rest, accumulator + decodedValue, multiplier * 128)
+    else
+      accumulator + decodedValue
+    end
+  end
     Message.ping_request
   end
 
--- test/hulaaki/decoder_test.exs
+++ test/hulaaki/decoder_test.exs
@@ -4,6 +4,44 @@ defmodule Hulaaki.DecoderTest do
   alias Hulaaki.Message, as: Message
   alias Hulaaki.Packet, as: Packet
 
+  test "decode remaining length from provided bytes" do
+    expected = 321
+    received = Decoder.decode_remaining_length(<<193, 2>>)
+    assert expected == received
+
+    expected = 0
+    received = Decoder.decode_remaining_length(<<0>>)
+    assert expected == received
+
+    expected = 127
+    received = Decoder.decode_remaining_length(<<127>>)
+    assert expected == received
+
+    expected = 128
+    received = Decoder.decode_remaining_length(<<128, 1>>)
+    assert expected == received
+
+    expected = 16_383
+    received = Decoder.decode_remaining_length(<<255, 127>>)
+    assert expected == received
+
+    expected = 16_384
+    received = Decoder.decode_remaining_length(<<128, 128, 1>>)
+    assert expected == received
+
+    expected = 2_097_151
+    received = Decoder.decode_remaining_length(<<255, 255, 127>>)
+    assert expected == received
+
+    expected = 2_097_152
+    received = Decoder.decode_remaining_length(<<128, 128, 128, 1>>)
+    assert expected == received
+
+    expected = 268_435_455
+    received = Decoder.decode_remaining_length(<<255, 255, 255, 127>>)
+    assert expected == received
+  end
+
   test "attempts to decode a ping request message" do
     message = Message.ping_request
     encoded_bytes = Packet.encode message
