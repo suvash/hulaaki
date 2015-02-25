a69e95a3430f63d2e07f271252860661f158722f
Suvash Thapaliya <suvash@gmail.com>
2015-02-25 01:31:00 +0100

    Fun names for clients in test

--- test/hulaaki/client_test.exs
+++ test/hulaaki/client_test.exs
@@ -7,10 +7,21 @@ defmodule Hulaaki.ClientTest do
     connection.socket |> Socket.Stream.send! packet
   end
 
+  defp client_name do
+    adjectives = [ "lazy", "funny", "bright", "boring", "crazy", "lonely" ]
+    nouns = [ "thermometer", "switch", "scale", "bulb", "heater", "microwave" ]
+
+    :random.seed(:os.timestamp)
+    [adjective] = adjectives |> Enum.shuffle |> Enum.take 1
+    [noun] = nouns |> Enum.shuffle |> Enum.take 1
+
+    adjective <> "-" <> noun
+  end
+
   test "establishes valid connection" do
     address = "localhost"
     port = 1883
-    client_id = "random-client-is-mad"
+    client_id = client_name
     connection = Client.connect(address, port, client_id)
 
     assert %Client.Connection{} = connection
