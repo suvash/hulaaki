928c78315729d920a68a7a5242f5f775a0d9023f
Suvash Thapaliya <suvash@gmail.com>
2015-03-31 15:26:24 +0200

    Test implementation for on_connect in Client

--- lib/hulaaki/client.ex
+++ lib/hulaaki/client.ex
@@ -2,6 +2,7 @@ defmodule Hulaaki.Client do
   defmacro __using__(_) do
     quote location: :keep do
       use GenEvent
+      alias Hulaaki.Message
 
       def start(initial_state) do
         {:ok, manager_pid} = GenEvent.start_link
@@ -9,14 +10,20 @@ defmodule Hulaaki.Client do
         {:ok, manager_pid}
       end
 
-      def handle_event(event, state) do
+      def handle_event(event = %Message.ConnAck{}, state) do
+        on_connect(event, state)
+        {:ok, state}
+      end
+
+      def handle_event(event, state) do   # Remove eventually
         on_event(event, state)
         {:ok, state}
       end
 
       def on_event(event, state)
+      def on_connect(event, state)        # Remove eventually
 
-      defoverridable [on_event: 2]
+      defoverridable [on_event: 2, on_connect: 2]
     end
   end
 end
--- test/hulaaki/client_test.exs
+++ test/hulaaki/client_test.exs
@@ -1,11 +1,16 @@
 defmodule Hulaaki.ClientTest do
   use ExUnit.Case
   use GenEvent
+  alias Hulaaki.Message
 
   defmodule TestClient do
     use Hulaaki.Client
 
     def on_event(event, state) do
+      Kernel.send state.parent, event
+    end
+
+    def on_connect(event, state) do
       Kernel.send state.parent, event
     end
 
@@ -22,4 +27,13 @@ defmodule Hulaaki.ClientTest do
     assert_receive {:amazing, :tuple}
   end
 
+  test "client receives connect_ack notification", %{client_pid: pid} do
+    session_present = 0
+    return_code = 3
+    message = Message.connect_ack(session_present, return_code)
+
+    pid |> GenEvent.sync_notify message
+
+    assert_receive %Hulaaki.Message.ConnAck{return_code: 3, session_present: 0}
+  end
 end
