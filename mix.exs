81453b7ae2e16680831c4afb3d0e82ec8d781710
Suvash Thapaliya <suvash@gmail.com>
2015-04-14 00:33:45 +0200

    Use ex_doc for documentation generation

--- mix.exs
+++ mix.exs
@@ -4,7 +4,10 @@ defmodule Hulaaki.Mixfile do
   def project do
     [app: :hulaaki,
      version: "0.0.1",
+     name: "Hulaaki",
      elixir: "~> 1.0",
+     source_url: "https://github.com/suvash/hulaaki",
+     homepage_url: "https://github.com/suvash/hulaaki",
      deps: deps,
      test_coverage: [tool: ExCoveralls]
     ]
@@ -28,6 +31,8 @@ defmodule Hulaaki.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [{:inch_ex, "~> 0.2.4", only: :docs},
+     {:earmark, "~> 0.1", only: [:dev, :docs]},
+     {:ex_doc, "~> 0.7", only: [:dev, :docs]},
      {:dialyze, "~> 0.1.3", only: :test},
      {:excoveralls, "~> 0.3", only: [:dev, :test]}]
   end
